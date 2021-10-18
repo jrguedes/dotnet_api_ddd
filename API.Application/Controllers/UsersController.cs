507940b4c65490e2d06007f2bd576d3bef8f8be7
Júnior Guedes <jrguedes.ja@gmail.com>
2021-10-18 18:16:38 -0300

    Post, GetAll e Get funcionando

--- API.Application/Controllers/UsersController.cs
+++ API.Application/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using System.Threading.Tasks;
+using API.Domain.Entities;
 using API.Domain.Interfaces.Services.User;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,7 +13,7 @@ namespace API.Application.Controllers
     {
         private readonly IUserService _service;
 
-        public UsersController(IUserService service)        
+        public UsersController(IUserService service)
         {
             _service = service;
         }
@@ -20,14 +21,46 @@ namespace API.Application.Controllers
         public async Task<ActionResult> Get()
         {
             try
-            {                
-                return Ok(await _service.GetAllAsync());                
+            {
+                return Ok(await _service.GetAllAsync());
             }
             catch (ArgumentException e)
             {
                 return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
             }
         }
+        
+        [HttpGet("{id}", Name = "GetById")]                
+        public async Task<ActionResult> Get(Guid id)
+        {
+            try
+            {
+                return Ok(await _service.GetAsync(id));
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [HttpPost]
+        public  async Task<ActionResult> Post([FromBody] UserEntity user)
+        {
+            try
+            {
+                 var result = await _service.PostAsync(user);
+                 if (result != null){
+                     //Retorna no Header o link de acesso ao objeto
+                     return Created(new Uri(Url.Link("GetById", new {id = result.Id})), result);
+                 }
+                 return  BadRequest();
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
 
     }
 }
