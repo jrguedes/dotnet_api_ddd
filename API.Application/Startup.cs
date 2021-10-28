4d2c234fd587eaf58e4d0ca274dfb57f0bfd57c3
Júnior Guedes <jrguedes.ja@gmail.com>
2021-10-28 09:25:24 -0300

    Adicionar Botão de Autenticação no Swagger
    Adicionar Botão de Autenticação no Swagger

--- API.Application/Startup.cs
+++ API.Application/Startup.cs
@@ -101,6 +101,30 @@ namespace application
                         Url = new Uri("http://www.google.com/minhalicenca")
                     }
                 });
+
+                //faz o botao de autenticacao aparecer
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Entre com o Token JWT",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                //faz a autenticacao
+                c.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement{
+                        {
+                            new OpenApiSecurityScheme{
+                                Reference = new OpenApiReference{
+                                    Id = "Bearer",
+                                    Type = ReferenceType.SecurityScheme
+                                }
+                            },
+                            new List<string>()
+                        }
+                    }
+                );
             });
         }
 
